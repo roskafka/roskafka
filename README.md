5bb8ca92341d19a42b06bd8b29f68838d6539f95
Alexander Grueneberg <git@agrueneberg.info>
2023-03-29 17:32:32 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# roskafka
+
+Build and run roskafka:
+
+    mkdir -p ~/ros2_ws/src
+    cd ~/ros2_ws/src
+    git clone https://gitlab.informatik.hs-furtwangen.de/ss23-forschungsprojekt-7/roskafka.git
+    cd ~/ros2_ws
+    PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources colcon build
+    . install/setup.bash
+    ros2 run roskafka bridge
---
+++ roskafka/bridge.py
@@ -0,0 +1,40 @@
+# Each import needs to be declared in package.xml
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from kafka import KafkaProducer
+
+
+class Bridge(Node):
+
+    def __init__(self):
+        super().__init__('bridge')
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.listener_callback,
+            10)
+        self.producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.producer.send('ros', msg.data.encode())
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+
+    bridge = Bridge()
+
+    rclpy.spin(bridge)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    bridge.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
