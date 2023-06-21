da03db8658f24da68983ca254c22ca7959d389ab
Alexander Grueneberg <git@agrueneberg.info>
2023-06-21 18:54:42 +0200

    Drop extra wait_for_schema

--- roskafka/utils.py
+++ roskafka/utils.py
@@ -18,15 +18,3 @@ def dict_to_msg(type_name, values):
     instance = get_msg_type(type_name)()
     rosidl_runtime_py.set_message_fields(instance, values)
     return instance
-
-
-def wait_for_schema(topic):
-    max_retries = 10
-    while max_retries > 0:
-        try:
-            return sr.get_latest_version(f"{topic}-value").schema
-        except Exception:
-            self.node.get_logger().debug(f"Waiting for schema for topic {topic} ...")
-            rclpy.spin_once(self.node, timeout_sec=1.0)
-            max_retries -= 1
-    raise Exception(f"Could not get schema for topic {topic}")
